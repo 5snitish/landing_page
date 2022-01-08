2f9768e3d7aa73a10671fa8b3c114260844ef0c4
nitish-maitretech <nitish@maitretech.com>
2022-01-08 20:00:19 +0530

    header fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ number, text_primary, text_secoundry }) => {
   return (
-    <div className="flex flex-col px-6  h-[134px] w-[368px] bg-white justify-center rounded-[25px] space-y-3 shadowDiv">
+    <div className="flex flex-col px-6  h-[134px] w-[320px] md:w-[368px] bg-white justify-center rounded-[25px] space-y-3 shadowDiv">
       <div className="flex flex-row space-x-2 items-center">
         <div className="flex items-center bg-btn-secoundry rounded-full h-9 w-9 justify-center">
           <span className="text-white font-bold text-2xl">{number}</span>
